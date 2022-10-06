a13258d6f1350cc429fcef47d3fea4d22f2a0322
LindaThao2 <93567275+LindaThao2@users.noreply.github.com>
2022-10-06 12:08:23 -0700

    Update Main.java
    Updated Main for Working Code

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,39 @@
-//Write a program that asks the user for the number of linear equations to solve (let’s say n <=10)
-// using the Gaussian elimination with Scaled Partial Pivoting method. Ask the user to first enter
-// the number of equations and then give them the choice to enter the coefficients from the command line
-// (by asking for each row that includes the b value) or have them enter a file name which has the
-// augmented coefficient matrix (including the b values) in a simple text file format as seen below for an
-// example of 3 equations :
-//
-//        E.g. the contents of a file for 3 linear equations 2x+3y = 8, -x+2y-z=0, 3x+2z=9 will be
-//        2 3 0 8
-//        -1 2 -1 0
-//        3 0 2 9
-//
-//        Your program should output the scaled ratios at each step, and mention the pivot row selected based on
-//        the scaled ratio. Show the intermediate matrix at each step of the Gaussian Elimination process.
-//        Finally, the final output of your program should be the solution in the following format :
-//        x=1
-//        y=2
-//        z=3
-
 import java.util.Scanner;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    // ASSUMING ZERO BASED INDEXING THEREFORE IN ARRAYS
+    // 1 to n ==> 0 to n-1
+
+    public static void main(String[] args) throws IOException {
+
+//        File file = new File("/Users/lindathao/IdeaProjects/cs3010_proj1_v2/src/test.txt");
+//
+//        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+//
+//        int n;
+//        String st ;
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        while ((st = bufferedReader.readLine()) != null) {
+//            arrayList.add(st);
+//        }
+//
+//        Scanner scanner = new Scanner(System.in);
+//        n = Integer.parseInt(arrayList.get(0));
+
 
         Scanner input = new Scanner(System.in);
 
         int numOfEqns, opt, coef;
-        String fileName = "";
 
         System.out.println("Please select an option: ");
         System.out.println(" 1. Manually Enter Coefficients for Equations");
@@ -38,19 +45,19 @@ public class Main {
         numOfEqns = input.nextInt();
 
         // checks to ensure that number of linear eqns is between 1 and 10
-        while (numOfEqns < 1 || numOfEqns > 10){
+        while (numOfEqns < 1 || numOfEqns > 10) {
             System.out.println("ERROR!");
             System.out.println("Please enter the number of linear equations ( between 1 and 10 )");
             numOfEqns = input.nextInt();
         }
 
         // array to store the coefficients
-        int[][] coeffArr = new int[numOfEqns][numOfEqns];
-        int[] bVal = new int[numOfEqns];
+        double[][] coeffArr = new double[numOfEqns][numOfEqns];
+        double[] bVal = new double[numOfEqns];
+        int[] sVector = new int[numOfEqns];
 //        int bVal = 0;
 
-
-        switch (opt){
+        switch (opt) {
 
             //    Give them the choice to enter the coefficients from the command line
             //         - (by asking for each row that includes the b value)
@@ -60,118 +67,112 @@ public class Main {
 
                 // i is row
                 // j is col
-                for(int i = 1; i < numOfEqns + 1; i++){
+                for (int i = 0; i < numOfEqns; i++) {
 
-                    for(int j = 1; j < numOfEqns + 1; j ++) {
+                    for (int j = 0; j < numOfEqns; j++) {
                         System.out.println("ROW " + i);
                         System.out.println("COL " + j);
                         System.out.println("Enter coefficient for X" + j);
-                        coeffArr[i - 1][j - 1] = input.nextInt();
+                        coeffArr[i][j] = input.nextInt();
                     }
 
                     System.out.println("Please enter b-value for the equation " + i + ": ");
-                        bVal[i - 1] = input.nextInt();
-
+                    bVal[i] = input.nextInt();
 
                 }
 
 
+//                double[][] coeff = new double[n][n];
+//                int k = 1;
+//                for (int i = 0; i < n; i++) {
+//                    String[] temp = arrayList.get(k++).split(" ");
+//                    for (int j = 0; j < n; j++) {
+//                        coeff[i][j] = Double.parseDouble(temp[j]);
+//                    }
+//                }
+//
+//                double[] vector = new double[n];
+//                String[] temp = arrayList.get(k).split(" ");
+//                for (int i = 0; i < n; i++) {
+//                    vector[i] = Double.parseDouble(temp[i]);
+//                }
+
+                Main main = new Main();
+                main.SPPGaussian(coeffArr, bVal);
+
+
                 break;
             case 2:
+                
                 break;
             default:
                 break;
-
         }
-
-        System.out.println("TEST PRINT:");
-
-        for(int i = 0; i < coeffArr.length; i++){
-
-            for (int j = 0; j < coeffArr.length; j++) {
-                System.out.println(coeffArr[i][j]);
-            }
-
-            System.out.println("b value: " + bVal[i]);
-
-        }
-
-
-
-//    Ask the user for the number of linear equations to solve (let’s say n <=10)
-//    Ask the user to first enter the number of equations
-
-
-//    or have them enter a file name which has the augmented coefficient matrix (including the b values)
-//    in a simple text file format as seen below for an
-
-
-
-        // New code that was discussed with alondra
-
-
-
-
-        // STEP 1:
-        // Find largest numbers in every row.
-        // Find S-vector
-
-        for(int i = 0; i < numOfEqns; i++){
-
-            // maxTemp will hold the temp max value, starting with first number in each row automatically set as the highest
-            int maxTemp[i] = cm[i][0];
-
-            // search through maxtrix
-            for(int j = 1; j < numOfEqns; j++){
-
-                // compare numbers within the rows to see which one is the largest
-                if (cm[i][j] > maxTemp
-                maxtemp = cm[i][j];
-            }
-
-            // Store all max temps that were found for each row into S array
-            s[i] = maxTemp;
-        }
-
-
-        // STEP 2: Find SR
-
-        int pivot_index = 0;
-        int pivot_value = -1;
-
-        for(int i = 0; i < sr.length; i++){
-
-            s[i] = (cm[i][j])/s[i];
-
-            //store the largest ration to help find the pivot row/number
-            if(s[i] > pivot_value){
-                pivot_value = s[i];
-                pivot_index = i;
-            }
-        }
-
-
-
-
-
 
     }
 
+    private void SPPGaussian(double[][] coeff, double[] vector) {
 
-    //STEP 3: populate the multiplier
-    public static void matrix_update(int sizeOfMultiplier, int pivotValue){
-
-        int[] multiplier = new int[sizeOfMultiplier];
-
-        for(int i; ){
-
-            cm[i][0] = multiplier[i] * pivotValue;
-
+        int n = coeff.length;
+        double[] sol = new double[n];
+        int[] ind = new int[n];
+        for (int i = 0; i < n; i++) {
+            ind[i] = i;
         }
+        SPPForwardElimination(coeff, vector, ind);
+        SPPBackSubst(coeff, vector, sol, ind);
 
-
-
+        System.out.println(Arrays.toString(sol));
     }
 
+    private void SPPForwardElimination(double[][] coeff, double[] vector, int[] ind) {
+        int n = coeff.length;
+        double[] scaling = new double[n];
 
+        for (int i = 0; i < n; i++) {
+            double smax = 0;
+            for (int j = 0; j < n; j++) {
+                smax = Math.max(smax, coeff[i][j]);
+            }
+            scaling[i] = smax;
+        }
+        for (int k = 0; k < n; k++) {
+            double rmax = 0;
+            int maxInd = k;
+
+            for (int i = k; i < n; i++) {
+                double r = coeff[ind[i]][k] / scaling[ind[i]];
+                if (r > rmax) {
+                    rmax = r;
+                    maxInd = i;
+                }
+            }
+            swap(ind, maxInd, k);
+
+            for (int i = k + 1; i < n; i++) {
+                double mult = coeff[ind[i]][k] / coeff[ind[k]][k];
+                for (int j = k + 1; j < n; j++) {
+                    coeff[ind[i]][j] = coeff[ind[i]][j] - mult * coeff[ind[k]][j];
+                }
+                vector[ind[i]] = vector[ind[i]] - mult * vector[ind[k]];
+            }
+        }
+    }
+
+    private void SPPBackSubst(double[][] coeff, double[] vector, double[] sol, int[] ind) {
+        int n = coeff.length;
+        for (int i = n - 1; i >= 0; i--) {
+            double sum = vector[ind[i]];
+            for (int j = i + 1; j < n; j++) {
+                sum = sum - coeff[ind[i]][j] * sol[j];
+            }
+            sol[i] = sum / coeff[ind[i]][i];
+        }
+    }
+
+    private void swap(int[] ind, int i, int j) {
+        int temp = ind[i];
+        ind[i] = ind[j];
+        ind[j] = temp;
+    }
 }
